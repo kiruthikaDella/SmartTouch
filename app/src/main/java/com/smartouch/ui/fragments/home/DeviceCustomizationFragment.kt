54b748903e9c4a7211c34031d47382ee00adb049
Jignesh Dangar <jignesh.dangar@teksun.com>
2021-04-20 15:18:41 +0530

    - Custom lock UI

--- app/src/main/java/com/smartouch/ui/fragments/home/DeviceCustomizationFragment.kt
+++ app/src/main/java/com/smartouch/ui/fragments/home/DeviceCustomizationFragment.kt
@@ -12,6 +12,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.smartouch.R
+import com.smartouch.common.utils.dialog
 import com.smartouch.databinding.FragmentDeviceCustomizationBinding
 
 class DeviceCustomizationFragment : Fragment() {
@@ -72,6 +73,12 @@ class DeviceCustomizationFragment : Fragment() {
 
                 }
 
+            }
+        }
+
+        binding.ibLock.setOnClickListener {
+            activity?.let {
+                dialog.askAlert(it,getString(R.string.dialog_title_text_lock),getString(R.string.text_ok),getString(R.string.text_cancel),null)
             }
         }
     }
