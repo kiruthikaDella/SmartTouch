b7096b5c46a26bf70be391780bc94f4436d18f3b
Binjal prajapati <binjal.prajapati@teksun.com>
2021-12-17 18:59:39 +0530

    - Added smartap panel ui

--- app/src/main/java/com/voinismartiot/voni/adapters/controlmodeadapter/ControlModeDeviceAdapter.kt
+++ app/src/main/java/com/voinismartiot/voni/adapters/controlmodeadapter/ControlModeDeviceAdapter.kt
@@ -10,6 +10,7 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.voinismartiot.voni.R
 import com.voinismartiot.voni.api.model.GetDeviceData
 import com.voinismartiot.voni.common.utils.Constants
@@ -17,7 +18,6 @@ import com.voinismartiot.voni.common.utils.Utils.toBoolean
 import com.voinismartiot.voni.common.utils.Utils.toInt
 import com.voinismartiot.voni.mqtt.AwsMqttSingleton
 import com.voinismartiot.voni.mqtt.MQTTConstants
-import com.google.android.material.switchmaterial.SwitchMaterial
 import com.warkiz.widget.IndicatorSeekBar
 import com.warkiz.widget.OnSeekChangeListener
 import com.warkiz.widget.SeekParams
@@ -59,7 +59,7 @@ class ControlModeDeviceAdapter(
             }
             else -> {
                 val v = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_control_mode_room_smart_ack, parent, false)
+                    .inflate(R.layout.item_control_mode_room_smart_ap, parent, false)
                 SmartApPanelViewHolder(v)
             }
         }
@@ -108,8 +108,20 @@ class ControlModeDeviceAdapter(
             }
         } else if (deviceList[position].productGroup.lowercase() == Constants.PRODUCT_SMART_ACK) {
             smartAckPanelView
+        } else if (deviceList[position].productGroup.lowercase() == Constants.PRODUCT_SMART_AP) {
+            when (deviceList[position].deviceType) {
+                Constants.DEVICE_TYPE_EIGHT -> {
+                    eightPanelView
+                }
+                Constants.DEVICE_TYPE_FOUR -> {
+                    fourPanelView
+                }
+                else -> {
+                    smartApPanelView
+                }
+            }
         } else {
-            smartApPanelView
+            0
         }
     }
 
@@ -202,29 +214,16 @@ class ControlModeDeviceAdapter(
     }
 
     inner class SmartApPanelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val relativeMain = itemView.findViewById(R.id.relative_main) as RelativeLayout
+        val constraintLayout = itemView.findViewById(R.id.relative_main) as RelativeLayout
         val relativeLayout = itemView.findViewById(R.id.relative_layout) as RelativeLayout
-
         val tvPanelName = itemView.findViewById(R.id.tv_panel_name) as TextView
+
+
         val tvSwitchNameOne = itemView.findViewById(R.id.tv_switch_one_name) as TextView
-        val tvSwitchNameTwo = itemView.findViewById(R.id.tv_switch_two_name) as TextView
-        val tvSwitchNameThree = itemView.findViewById(R.id.tv_switch_three_name) as TextView
-        val tvSwitchNameFour = itemView.findViewById(R.id.tv_switch_four_name) as TextView
-        val tvSwitchNameFive = itemView.findViewById(R.id.tv_switch_five_name) as TextView
 
         val tvSwitchNameOneDesc = itemView.findViewById(R.id.tv_switch_one_type) as TextView
-        val tvSwitchNameTwoDesc = itemView.findViewById(R.id.tv_switch_two_type) as TextView
-        val tvSwitchNameThreeDesc = itemView.findViewById(R.id.tv_switch_three_type) as TextView
-        val tvSwitchNameFourDesc = itemView.findViewById(R.id.tv_switch_four_type) as TextView
-        val tvSwitchNameFiveDesc = itemView.findViewById(R.id.tv_switch_five_type) as TextView
 
         val switchOne = itemView.findViewById(R.id.switch_one) as SwitchMaterial
-        val switchTwo = itemView.findViewById(R.id.switch_two) as SwitchMaterial
-        val switchThree = itemView.findViewById(R.id.switch_three) as SwitchMaterial
-        val switchFour = itemView.findViewById(R.id.switch_four) as SwitchMaterial
-        val switchFive = itemView.findViewById(R.id.switch_five) as SwitchMaterial
-
-        val seekBar = itemView.findViewById(R.id.seek_bar) as IndicatorSeekBar
 
     }
 
@@ -693,12 +692,13 @@ class ControlModeDeviceAdapter(
         holder.apply {
 
             try {
+
                 val wrapSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-                relativeMain.measure(wrapSpec, wrapSpec)
+                constraintLayout.measure(wrapSpec, wrapSpec)
 
                 val relativeParams = relativeLayout.layoutParams as RelativeLayout.LayoutParams
 
-                relativeParams.height = relativeMain.measuredHeight
+                relativeParams.height = constraintLayout.measuredHeight
                 relativeParams.width = RelativeLayout.LayoutParams.MATCH_PARENT
 
                 relativeLayout.layoutParams = relativeParams
@@ -706,13 +706,12 @@ class ControlModeDeviceAdapter(
                 e.printStackTrace()
             }
 
+            tvPanelName.text = device.deviceName
             if (device.isDeviceAvailable == "0") {
                 relativeLayout.visibility = View.VISIBLE
             } else {
                 relativeLayout.visibility = View.GONE
             }
-
-            tvPanelName.text = device.deviceName
 
             device.switchData?.let { switchData ->
                 for (value in switchData) {
@@ -725,46 +724,6 @@ class ControlModeDeviceAdapter(
                                 tvSwitchNameOneDesc.text = it
                             }
                         }
-                        "2" -> {
-                            val switchName = value.name
-                            tvSwitchNameTwo.text = switchName
-                            switchTwo.isChecked = value.switchStatus.toInt().toBoolean()
-                            value.desc?.let {
-                                tvSwitchNameTwoDesc.text = it
-                            }
-                        }
-                        "3" -> {
-                            val switchName = value.name
-                            tvSwitchNameThree.text = switchName
-                            switchThree.isChecked = value.switchStatus.toInt().toBoolean()
-                            value.desc?.let {
-                                tvSwitchNameThreeDesc.text = it
-                            }
-                        }
-                        "4" -> {
-                            val switchName = value.name
-                            tvSwitchNameFour.text = switchName
-                            switchFour.isChecked = value.switchStatus.toInt().toBoolean()
-                            value.desc?.let {
-                                tvSwitchNameFourDesc.text = it
-                            }
-                        }
-                        "5" -> {
-                            val switchName = value.name
-                            tvSwitchNameFive.text = switchName
-                            switchFive.isChecked = value.switchStatus.toInt().toBoolean()
-                            value.desc?.let {
-                                tvSwitchNameFiveDesc.text = it
-                            }
-                        }
-                        "6" -> {
-                            try {
-                                seekBar.setProgress(value.switchStatus.toFloat())
-                            } catch (e: Exception) {
-                                e.printStackTrace()
-                            }
-
-                        }
                     }
                 }
             }
@@ -775,56 +734,6 @@ class ControlModeDeviceAdapter(
                     MQTTConstants.AWS_SWITCH_1,
                     switchOne.isChecked.toInt().toString()
                 )
-            }
-
-            switchTwo.setOnClickListener {
-                publish(
-                    device.deviceSerialNo,
-                    MQTTConstants.AWS_SWITCH_2,
-                    switchTwo.isChecked.toInt().toString()
-                )
-            }
-
-            switchThree.setOnClickListener {
-                publish(
-                    device.deviceSerialNo,
-                    MQTTConstants.AWS_SWITCH_3,
-                    switchThree.isChecked.toInt().toString()
-                )
-            }
-
-            switchFour.setOnClickListener {
-                publish(
-                    device.deviceSerialNo,
-                    MQTTConstants.AWS_SWITCH_4,
-                    switchFour.isChecked.toInt().toString()
-                )
-            }
-
-            switchFive.setOnClickListener {
-                publish(
-                    device.deviceSerialNo,
-                    MQTTConstants.AWS_SWITCH_5,
-                    switchFive.isChecked.toInt().toString()
-                )
-            }
-
-            seekBar.onSeekChangeListener = object : OnSeekChangeListener {
-                override fun onSeeking(seekParams: SeekParams?) {
-                }
-
-                override fun onStartTrackingTouch(seekBar: IndicatorSeekBar?) {
-                }
-
-                override fun onStopTrackingTouch(seekBar: IndicatorSeekBar?) {
-                    seekBar?.progress?.let {
-                        publishDimmer(
-                            device.deviceSerialNo,
-                            it.toString()
-                        )
-                    }
-                }
-
             }
         }
     }
@@ -901,7 +810,29 @@ class ControlModeDeviceAdapter(
                                         jsonObject.getString(
                                             MQTTConstants.AWS_USB_C
                                         ) //USB C
-                                } else {
+                                } else if (jsonObject.getString(MQTTConstants.AWS_DEVICE_TYPE) == "4") {
+                                    /*if (jsonObject.has(MQTTConstants.AWS_USB_C)) {
+                                        deviceData?.switchData?.get(4)?.switchStatus =
+                                            jsonObject.getString(
+                                                MQTTConstants.AWS_DIMMER
+                                            ) //Dimmer
+                                        deviceData?.switchData?.get(5)?.switchStatus =
+                                            jsonObject.getString(
+                                                MQTTConstants.AWS_USB_C
+                                            ) //USB C
+                                    } else {
+                                        deviceData?.switchData?.get(4)?.switchStatus =
+                                            switchStatus[4] //Fan speed
+
+                                        deviceData?.switchData?.get(5)?.switchStatus =
+                                            jsonObject.getString(
+                                                MQTTConstants.AWS_DIMMER
+                                            ) //Dimmer
+                                    }*/
+                                    deviceData?.switchData?.get(0)?.switchStatus = switchStatus[0]
+                                    deviceData?.switchData?.get(1)?.switchStatus = switchStatus[1]
+                                    deviceData?.switchData?.get(2)?.switchStatus = switchStatus[2]
+                                    deviceData?.switchData?.get(3)?.switchStatus = switchStatus[3]
                                     if (jsonObject.has(MQTTConstants.AWS_USB_C)) {
                                         deviceData?.switchData?.get(4)?.switchStatus =
                                             jsonObject.getString(
@@ -920,6 +851,8 @@ class ControlModeDeviceAdapter(
                                                 MQTTConstants.AWS_DIMMER
                                             ) //Dimmer
                                     }
+                                } else if (jsonObject.getString(MQTTConstants.AWS_DEVICE_TYPE) == "1") {
+                                    deviceData?.switchData?.get(0)?.switchStatus = switchStatus[0]
                                 }
                             }
                         }
